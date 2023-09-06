0e6ab4bb21907cecb24f5e94a1ab27a1e07bcbf0
vipvenkat <95736679+vipvenkat@users.noreply.github.com>
2023-09-06 12:02:29 +0530

    Update server.test.js

--- server.test.js
+++ server.test.js
@@ -1,6 +1,18 @@
-const sayHello = require('./server');
+const request = require('supertest');
+const server = require('./server');
 
-test('should return "Hello, World!"', () => {
-  const result = sayHello();
-  expect(result).toBe('Hello, World!');
+describe('Server tests', () => {
+  afterAll((done) => {
+    server.close();
+    done();
+  });
+  test('GET /todos', async () => {
+    const response = await request(server).get('/todos').send();
+    expect(response.statusCode).toBe(200);
+    expect(response.body.error).toBe(null);
+    expect(response.body.data.length).toBe(3);
+    expect(response.body.data[0]).toBe('This is a task');
+    expect(response.body.data[1]).toBe('This is another task');
+    expect(response.body.data[2]).toBe('Again another task');
+  });
 });
